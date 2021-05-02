bfca306b1b1618b5c14bd838ddf2f5e9348f8873
Simon Billinge <simon.billinge@gmail.com>
2021-05-02 12:30:25 -0400

    setting up better test rig for io

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,18 +1,28 @@
 from pathlib import Path
+
+import CifFile
+import pytest
 from testfixtures import TempDirectory
 
 from cifplotting.io import cif_read
-from tests.inputs.test_cif_contents import cif_contents_string
+from tests.inputs.test_cifs import testciffiles_contents_expecteds
 
-
-def test_cif_read():
-    cif_bitstream = bytearray(cif_contents_string, 'utf8')
+@pytest.mark.parametrize("cm", testciffiles_contents_expecteds)
+def test_cif_read(cm):
     with TempDirectory() as d:
-        d.write('test_cif.cif',
-                cif_bitstream)
         temp_dir = Path(d.path)
-        test_cif_path = temp_dir / 'test_cif.cif'
+        cif_bitstream = bytearray(cm[0], 'utf8')
+        d.write(f"test_cif.cif",
+                cif_bitstream)
+        test_cif_path = temp_dir / f"test_cif.cif"
         actual = cif_read(test_cif_path)
-    expected = {"_diffrn_radiation_wavelength": 1.5482, 
-                "_diffrn_radiation_probe": "neutrons"}
-    assert actual == expected
+    expected = CifFile.CifFile()
+    expected[cm[1].get('block_name')] = CifFile.CifBlock()
+    cb = expected[cm[1].get('block_name')]
+    for item in cm[1].get('block_items'):
+        cb[item[0]] = item[1]
+    for item in cm[1].get('loop_items'):
+        cb.AddCifItem(([item['keys']],[item['values']]))
+    assert actual.keys() == expected.keys()
+    assert actual[cm[1].get('block_name')].items() == expected[cm[1].get('block_name')].items()
+
