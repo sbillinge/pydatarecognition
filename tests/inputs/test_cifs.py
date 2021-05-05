96617483623697c9e4e244439ea825b50ad58a4f
Simon Billinge <simon.billinge@gmail.com>
2021-05-05 14:22:33 -0400

    pep8

--- tests/inputs/test_cifs.py
+++ tests/inputs/test_cifs.py
@@ -11,7 +11,7 @@
 #    pairs and loop items are themselves a dict containing lists of keys and values
 
 testciffiles_contents_expecteds = [
-("\
+    ("\
 data_cubic_1_ND\n\
 \n\
 _diffrn_radiation_probe                neutrons\n\
@@ -28,23 +28,23 @@ _pd_proc_intensity_bkg_calc\n\
    178    10.0913    2212(172)          1886.0148         1886.0148\n\
    179    10.1413    2155(169)          1886.0148         1886.0148\n\
 "
-,
-{'block_name': 'cubic_1_ND',
-    "block_items": [("_diffrn_radiation_probe", "neutrons"),
-                    ('_diffrn_radiation_wavelength', '1.5482')
-                    ],
-    "loop_items": [{"keys": ["_pd_proc_point_id",
-                             "_pd_proc_2theta_corrected",
-                             "_pd_proc_intensity_total",
-                             "_pd_calc_intensity_total",
-                             "_pd_proc_intensity_bkg_calc"],
-                  "values": [["177", "178", "179"],
-                             ["10.0413","10.0913","10.1413"],
-                             ["2037(166)","2212(172)","2155(169)"],
-                             ["1886.0148","1886.0148","1886.0148"],
-                             ["1886.0148","1886.0148","1886.0148"]
-                              ]
-                  }
-                         ]}
- )
+     ,
+     {'block_name': 'cubic_1_ND',
+      "block_items": [("_diffrn_radiation_probe", "neutrons"),
+                      ('_diffrn_radiation_wavelength', '1.5482')
+                      ],
+      "loop_items": [{"keys": ["_pd_proc_point_id",
+                               "_pd_proc_2theta_corrected",
+                               "_pd_proc_intensity_total",
+                               "_pd_calc_intensity_total",
+                               "_pd_proc_intensity_bkg_calc"],
+                      "values": [["177", "178", "179"],
+                                 ["10.0413", "10.0913", "10.1413"],
+                                 ["2037(166)", "2212(172)", "2155(169)"],
+                                 ["1886.0148", "1886.0148", "1886.0148"],
+                                 ["1886.0148", "1886.0148", "1886.0148"]
+                                 ]
+                      }
+                     ]}
+     )
 ]
