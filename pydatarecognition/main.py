1c1586456084f15538d1b1c629ff601bd1194f12
Berrak Ozer <bo2220@columbia.edu>
2021-08-05 22:40:07 +0300

    added argparse library

--- pydatarecognition/main.py
+++ pydatarecognition/main.py
@@ -6,6 +6,7 @@ from skbeam.core.utils import twotheta_to_q
 from pydatarecognition.cif_io import cif_read, rank_write, user_input_read
 from pydatarecognition.utils import xy_resample
 from pydatarecognition.plotters import rank_plot
+import argparse
 
 ############################################################################################
 TESTFILE = 3 # FIXME Use cli to parse this information instead.
@@ -38,6 +39,11 @@ STEPSIZE_REGULAR_QGRID = 10**-3
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--jsonify', action='store_true')
+    group.add_argument('--rank', action='store_true')
+    args = parser.parse_args()
     # These need to be inside main for this to run from an IDE like PyCharm
     # and still find the example files.
     PARENT_DIR = Path.cwd()
@@ -67,51 +73,57 @@ def main():
     cifname_ranks, r_pearson_ranks, doi_ranks = [], [], []
     user_dict, cif_dict = {}, {}
     print('Working with CIFs:')
-    for ciffile in ciffiles:
-        print(ciffile.name)
-        ciffile_path = Path(ciffile)
-        json_data = cif_read_ext(ciffile_path, 'json')
-        pre, ext = os.path.splitext(ciffile.name)
-        json_dump(json_data, str(OUTPUT_DIR/pre) + ".json")
-    #     pcd = cif_read(ciffile_path)
-    #     try:
-    #         data_resampled = xy_resample(user_q, user_intensity, pcd.q, pcd.intensity, STEPSIZE_REGULAR_QGRID)
-    #         pearson = scipy.stats.pearsonr(data_resampled[0][:,1], data_resampled[1][:,1])
-    #         r_pearson = pearson[0]
-    #         p_pearson = pearson[1]
-    #         cifname_ranks.append(ciffile.stem)
-    #         r_pearson_ranks.append(r_pearson)
-    #         doi = doi_dict[pcd.iucrid]
-    #         doi_ranks.append(doi)
-    #         cif_dict[str(ciffile.stem)] = dict([
-    #                     ('intensity', pcd.intensity),
-    #                     ('q', pcd.q),
-    #                     ('qmin', np.amin(pcd.q)),
-    #                     ('qmax', np.amax(pcd.q)),
-    #                     ('q_reg', data_resampled[1][:,0]),
-    #                     ('intensity_resampled', data_resampled[1][:,1]),
-    #                     ('r_pearson', r_pearson),
-    #                     ('p_pearson', p_pearson),
-    #                     ('doi', doi),
-    #                 ])
-    #     except AttributeError:
-    #         print(f"{ciffile.name} was skipped.")
-    # user_dict[str(user_input.stem)] = dict([
-    #     ('twotheta', userdata[:, 0]),
-    #     ('intensity', userdata[:, 1]),
-    #     ('q', user_q),
-    #     ('q_min', user_qmin),
-    #     ('q_max', user_qmax),
-    # ])
-    # cif_rank_pearson = sorted(list(zip(cifname_ranks, r_pearson_ranks, doi_ranks)), key = lambda x: x[1], reverse=True)
-    # ranks = [{'IUCrCIF': cif_rank_pearson[i][0],
-    #           'score': cif_rank_pearson[i][1],
-    #           'doi': cif_rank_pearson[i][2]} for i in range(len(cif_rank_pearson))]
-    # rank_txt = rank_write(ranks, OUTPUT_DIR)
-    # print(f'{frame_dashchars}{newline_char}{rank_txt}{frame_dashchars}')
-    # rank_plots = rank_plot(data_resampled[0][:,0], data_resampled[0][:, 1], cif_rank_pearson, cif_dict, OUTPUT_DIR)
-    # print(f'A txt file with rankings has been saved to the txt directory,{newline_char}'
-    #       f'and a plot has been saved to the png directory.{newline_char}{frame_dashchars}')
+    if args.jsonify:
+        for ciffile in ciffiles:
+            print(ciffile.name)
+            ciffile_path = Path(ciffile)
+            json_data = cif_read_ext(ciffile_path, 'json')
+            pre, ext = os.path.splitext(ciffile.name)
+            json_dump(json_data, str(OUTPUT_DIR/pre) + ".json")
+
+    if args.rank:
+        for ciffile in ciffiles:
+            print(ciffile.name)
+            ciffile_path = Path(ciffile)
+            pcd = cif_read(ciffile_path)
+            try:
+                data_resampled = xy_resample(user_q, user_intensity, pcd.q, pcd.intensity, STEPSIZE_REGULAR_QGRID)
+                pearson = scipy.stats.pearsonr(data_resampled[0][:,1], data_resampled[1][:,1])
+                r_pearson = pearson[0]
+                p_pearson = pearson[1]
+                cifname_ranks.append(ciffile.stem)
+                r_pearson_ranks.append(r_pearson)
+                doi = doi_dict[pcd.iucrid]
+                doi_ranks.append(doi)
+                cif_dict[str(ciffile.stem)] = dict([
+                            ('intensity', pcd.intensity),
+                            ('q', pcd.q),
+                            ('qmin', np.amin(pcd.q)),
+                            ('qmax', np.amax(pcd.q)),
+                            ('q_reg', data_resampled[1][:,0]),
+                            ('intensity_resampled', data_resampled[1][:,1]),
+                            ('r_pearson', r_pearson),
+                            ('p_pearson', p_pearson),
+                            ('doi', doi),
+                        ])
+            except AttributeError:
+                print(f"{ciffile.name} was skipped.")
+        user_dict[str(user_input.stem)] = dict([
+            ('twotheta', userdata[:, 0]),
+            ('intensity', userdata[:, 1]),
+            ('q', user_q),
+            ('q_min', user_qmin),
+            ('q_max', user_qmax),
+        ])
+        cif_rank_pearson = sorted(list(zip(cifname_ranks, r_pearson_ranks, doi_ranks)), key = lambda x: x[1], reverse=True)
+        ranks = [{'IUCrCIF': cif_rank_pearson[i][0],
+                  'score': cif_rank_pearson[i][1],
+                  'doi': cif_rank_pearson[i][2]} for i in range(len(cif_rank_pearson))]
+        rank_txt = rank_write(ranks, OUTPUT_DIR)
+        print(f'{frame_dashchars}{newline_char}{rank_txt}{frame_dashchars}')
+        rank_plots = rank_plot(data_resampled[0][:,0], data_resampled[0][:, 1], cif_rank_pearson, cif_dict, OUTPUT_DIR)
+        print(f'A txt file with rankings has been saved to the txt directory,{newline_char}'
+              f'and a plot has been saved to the png directory.{newline_char}{frame_dashchars}')
     return None
 
 if __name__ == "__main__":
