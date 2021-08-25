c1c8f73b3e2ace8995206e19071ed5d73f1df0bc
zthatch <zachary.alan.thatcher@gmail.com>
2021-08-25 03:16:53 -0400

    RBS: rebased onto async

--- pydatarecognition/app.py
+++ pydatarecognition/app.py
@@ -4,32 +4,56 @@ import yaml
 import tempfile
 import shutil
 import uuid
+import io
+import base64
 import asyncio
 from asyncio import BoundedSemaphore
 
-from fastapi import FastAPI, Body, HTTPException, status, File
+from fastapi import FastAPI, Body, HTTPException, status, File, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from starlette.config import Config as Configure
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, RedirectResponse
+
+from authlib.integrations.starlette_client import OAuth
+
 from typing import List, Optional, Literal
+
 import motor.motor_asyncio
+
 from pydatarecognition.powdercif import PydanticPowderCif
 from pydatarecognition.utils import xy_resample
 from pydatarecognition.cif_io import user_input_read
+
 from skbeam.core.utils import twotheta_to_q
+
 import scipy.stats
+
 import numpy as np
 
 import psutil
+
+filepath = Path(os.path.abspath(__file__))
 
 STEPSIZE_REGULAR_QGRID = 10**-3
 
 COLLECTION = "cif"
 MAX_MONGO_FIND = 1000000
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(SessionMiddleware, secret_key='!secret')
+templates = Jinja2Templates(directory="templates")
 
 # Connect to mongodb
-filepath = Path(os.path.abspath(__file__))
+
 with open(os.path.join(filepath.parent, 'secret_password.yml'), 'r') as f:
     user_secrets = yaml.safe_load(f)
 username = user_secrets['username']
@@ -48,21 +72,112 @@ for i in range(len(dois)):
 # Create an app level semaphore to prevent overloading the RAM. Assume ~100KB per cif, *5000 = 0.5GB
 semaphore = BoundedSemaphore(5000)
 
+@app.route('/')
+async def home(request: Request):
+    # user = request.session.get('user')
+    # if user is not None:
+    #     email = user['email']
+    #     html = (
+    #         f'<pre>Email: {email}</pre><br>'
+    #         '<a href="/docs">documentation</a><br>'
+    #         '<a href="/logout">logout</a>'
+    #     )
+    #     return HTMLResponse(html)
+    if request.session.get('login_status'):
+        if request.session['login_status'] == "authorized":
+            return templates.TemplateResponse('landing.html',
+                                              {"request": request, "user": request.session.get('username'), "img": request.session.get('photourl')})
+        else:
+            return templates.TemplateResponse('landing.html', {"request": request, "user": None})
+    else:
+         return templates.TemplateResponse('landing.html', {"request": request, "user": None})
 
-@app.post("/", response_description="Add new CIF", response_model=PydanticPowderCif)
-async def create_cif(powdercif: PydanticPowderCif = Body(...)):
-    powdercif = jsonable_encoder(powdercif)
-    new_cif = await db[COLLECTION].insert_one(powdercif)
-    created_cif = await db[COLLECTION].find_one({"_id": new_cif.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_cif)
+# Initialize our OAuth instance from the client ID and client secret specified in our .env file
+config = Configure('.env')
+oauth = OAuth(config)
 
-
-@app.get(
-    "/", response_description="List all cifs", response_model=List[PydanticPowderCif]
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
 )
-async def list_cifs():
-    cifs = await db[COLLECTION].find().to_list(5)
-    return cifs
+
+@app.get('/login', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def login(request: Request):
+
+    return templates.TemplateResponse('login.html')
+
+
+@app.get('/google_login', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def google_login(request: Request):
+    # Redirect Google OAuth back to our application
+    redirect_uri = request.url_for('auth')
+
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth')
+async def auth(request: Request):
+    # Perform Google OAuth
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+
+    # Save the user
+    request.session['user'] = dict(user)
+    request.session['photourl'] = request.session['user']['picture']
+    request.session['username'] = request.session['user']['given_name'] if ('given_name' in request.session['user']) else "Anonymous"
+    request.session['login_status'] = 'authorized'
+
+    return RedirectResponse(url='/')
+
+
+@app.get('/logout', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def logout(request: Request):
+    # Remove the user
+    request.session.pop('user', None)
+
+    return RedirectResponse(url='/')
+
+
+# @app.post("/", response_description="Add new CIF", response_model=PydanticPowderCif)
+# async def create_cif(powdercif: PydanticPowderCif = Body(...)):
+#     powdercif = jsonable_encoder(powdercif)
+#     new_cif = await db[COLLECTION].insert_one(powdercif)
+#     created_cif = await db[COLLECTION].find_one({"_id": new_cif.inserted_id})
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_cif)
+
+
+# @app.get(
+#     "/", response_description="List all cifs", response_model=List[PydanticPowderCif]
+# )
+# async def list_cifs():
+#     cifs = await db[COLLECTION].find().to_list(5)
+#     return cifs
+
+# Try to get the logged in user
+async def get_user(request: Request) -> Optional[dict]:
+    user = request.session.get('user')
+    if user is not None:
+        return user
+    else:
+        raise HTTPException(status_code=403, detail='Could not validate credentials.')
+
+    return None
+
+
+@app.route('/openapi.json')
+async def get_open_api_endpoint(request: Request, user: Optional[dict] = Depends(get_user)):  # This dependency protects our endpoint!
+    response = JSONResponse(get_openapi(title='FastAPI', version=1, routes=app.routes))
+    return response
+
+
+@app.get('/docs', tags=['documentation'])  # Tag it as "documentation" for our docs
+async def get_documentation(request: Request, user: Optional[dict] = Depends(get_user)):  # This dependency protects our endpoint!
+    response = get_swagger_ui_html(openapi_url='/openapi.json', title='Documentation')
+    return response
 
 
 @app.get(
@@ -153,6 +268,50 @@ async def rank_cif(xtype: Literal["twotheta", "q"], wavelength: float, user_inpu
     shutil.rmtree(tempdir)
     return ranks
 
+@app.route('/cif_search')
+async def cif_search(request: Request, user: Optional[dict] = Depends(get_user)):
+    """
+        Route function for cif search.
+
+        Returns
+        -------
+        render_template
+            Renders the cif search page.
+        """
+    return templates.TemplateResponse('cif_search.html',
+                                      {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl')})
+
+@app.route('/upload_data_cif', methods=['POST'])
+def upload_data_cif(request: Request, user: Optional[dict] = Depends(get_user)):
+    file = request.file.getlist('file')
+    wavelength = request.form['wavelength']
+    xtype = request.form['xtype']
+
+    return request.url_for('prepare_data_viz_NMF')
+
+
+@app.route('/prepare_data_viz_cif')
+def prepare_data_viz_cif(request: Request, user: Optional[dict] = Depends(get_user)):
+    pass
+
+
+@app.route('/cif_results')
+async def data_viz_cif_search(request: Request, user: Optional[dict] = Depends(get_user)):
+    # fig1 = plotting
+    # creates an in-memory buffer in which to store the file
+    file_object1 = io.BytesIO()
+    # fig1.savefig(file_object1, format='png')
+    # encodes byte object so it can be embedded directly in html
+    base64img1 = "data:image/png;base64," + base64.b64encode(file_object1.getvalue()).decode('ascii')
+    return templates.TemplateResponse('cif_search_visualization.html',
+                                      {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl'),
+                                       "base64img1": base64img1})
+
+# @app.route('/results_download')
+# async def comparison_download(request: Request, user: Optional[dict] = Depends(get_user)):
+#     return templates.TemplateResponse('cif_search_visualization.html',
+#                                       {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl')})
+
 
 async def limited_cif_load(cif: dict):
     await semaphore.acquire()
@@ -160,7 +319,48 @@ async def limited_cif_load(cif: dict):
     await pcd.resolve_gcs_tokens()
     return pcd
 
+@app.route('/about')
+def footer_about(request: Request):
+    """
+    Route function for about in the footer.
+
+    Returns
+    -------
+    render_template
+        Renders the footer-about page.
+    """
+    return templates.TemplateResponse('footer-about.html',
+                                      {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl')})
+
+
+@app.route('/privacy')
+def footer_privacy(request: Request):
+    """
+    Route function for privacy policy in the footer.
+
+    Returns
+    -------
+    render_template
+        Renders the privacy-policy page.
+    """
+    return templates.TemplateResponse('footer-privacy.html',
+                                      {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl')})
+
+
+@app.route('/terms')
+async def footer_term(request: Request):
+    """
+    Route function for terms of use in the footer.
+
+    Returns
+    -------
+    render_template
+        Renders the footer-term page.
+    """
+    return templates.TemplateResponse('footer-term.html',
+                                      {"request": request, "user": request.session.get('user'), "img": request.session.get('photourl')})
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="localhost", reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
